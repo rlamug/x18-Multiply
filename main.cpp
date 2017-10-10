cc2a1d46d8290834e98892ae5ce879c9bbccc115
rlamug <rlamug@mail.csuchico.edu>
2017-10-09 18:49:18 -0700

    Final

---
+++ main.cpp
@@ -0,0 +1,32 @@
+nclude <vector>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+vector< vector <int> > mult;
+mult.resize(13);
+for (int i = 0; i < mult.size(); i++){
+    mult[i].resize(13);
+    }
+    mult[0][0] = 0;
+    for (int i = 1; i <mult.size(); i++){
+        mult[i][0] = i;
+            mult[0][i] = i;
+            }
+            for (int i = 1; i < mult.size(); i++){
+
+                for (int o = 1; o < mult[i].size(); o++){
+                        mult[i][o] = i * o;
+                            }
+                            }
+                            for (int i = 0; i < mult.size(); i++){
+                                for (int o = 0; o < mult[i].size(); o++){
+                                        cout<<mult[i][o]<<" ";
+                                            }
+                                                cout<<endl;
+                                                }
+                                                return 0;
+                                                }
